e2871c9b03c82953f7c6c0bedb08f0430dc40b4f
Jacob Sides <jsides@pyxl.com>
2021-11-08 17:09:21 -0800

    TailwindCSS 2+ and Postcss support

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,34 @@
 <script>
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+  import logo from "./assets/svelte.png";
+  import Counter from "./lib/Counter.svelte";
 </script>
 
-<main>
+<main class="pattern-grid-lg text-green-500 bg-black min-h-screen">
   <img src={logo} alt="Svelte Logo" />
-  <h1>Hello world!</h1>
-
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+  <div class="m-24 p-6 lg:p-12 bg-green-900 pattern-grid-sm">
+	  <div class="text-white">
+	  	<h1 class="text-[64px] lg:text-[200px]">Hell world!</h1>
+	  	<Counter />
+	  	<p>
+	  			Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
+	  			apps.
+	  	</p>
+	  	<p>
+	  			Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
+	  			the officially supported framework, also powered by Vite!
+	  	</p>
+	  </div>
+  </div>
 </main>
 
-<style>
+<style lang="postcss">
+	@import '../node_modules/pattern.css/dist/pattern.min.css';
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
   :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   }
 
   main {
@@ -38,9 +43,7 @@
   }
 
   h1 {
-    color: #ff3e00;
     text-transform: uppercase;
-    font-size: 4rem;
     font-weight: 100;
     line-height: 1.1;
     margin: 2rem auto;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  mode: "jit",
+  purge: ['./index.html', './src/**/*.{svelte,js,ts}'],
+
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
